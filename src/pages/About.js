490125fe98dbc225d8d47f137b09f606a308e3d8
Mohammad Mhameed <mohmed6535@gmail.com>
2023-10-23 20:33:10 +0300

    upload project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="text-center bg-red-400 mx-auto">
+      <h1>about1</h1>
+      <h1>about2</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-center bg-blue-300">
+      <h1>home1</h1>
+      <h1>home2</h1>
+ </div>
+  );
+}
+
+export default Home;
